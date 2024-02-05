aa956e007fec14b5c78809be5e944255fdf209da
Akhmatdinov Ilgam <ilgamfa@yandex.ru>
2024-02-05 22:12:28 +0300

    IOS-23: Add MapKit

--- PatternCoordinator/MainTabBar/Courts/CourtsViewController.swift
+++ PatternCoordinator/MainTabBar/Courts/CourtsViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import MapKit
 
 final class CourtsViewController: UIViewController {
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Courts"
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(mapView)
+
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 }
